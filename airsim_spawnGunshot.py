b15058518ace09adf85a6b9dd47cac10ee4a0125
Jeremy I <sirunknown91@gmail.com>
2025-03-25 14:38:44 -0500

    gunshot audio

--- airsim_spawnGunshot.py
+++ airsim_spawnGunshot.py
@@ -1,14 +1,16 @@
 import airsim
 import time
 from threading import Thread
+from playsound import playsound
+from pathlib import Path
+import random
 
 def simSpawnGunshot(client : airsim.MultirotorClient, pos):
+    global gunshotCount
     pose = airsim.Pose(position_val=pos)
     scale = airsim.Vector3r(1, 1, 1)
     new_light_name = client.simSpawnObject("GunshotLight", "GunshotLightBase2", pose, scale, False, True)
     print("spawned gunshot")
-    time.sleep(.1)
-    client.simDestroyObject(new_light_name)
 
     dronePose = client.simGetVehiclePose()
     dist = pose.position.distance_to(dronePose.position)
@@ -16,8 +18,8 @@ def simSpawnGunshot(client : airsim.MultirotorClient, pos):
     soundTime = dist/343
     client.simPrintLogMessage(f"Spawned gunshot!", f"   Distance: {round(dist,2)} meters away. Should play sound after {round(soundTime,2)} seconds")
     time.sleep(soundTime)
-    print(f"bang!")
-
+    playsound("C:\\Users\\sirun\\Documents\\grad school stuff\\Research Assistant Work\\dronesim_research\\gunshotaudio\\1.wav", block=False)
+    
 if __name__ == '__main__':
     client = airsim.MultirotorClient()
     client.confirmConnection()
@@ -26,5 +28,9 @@ if __name__ == '__main__':
         dronePose = client.simGetVehiclePose()
         gunshotPos = airsim.Vector3r(dronePose.position.x_val, dronePose.position.y_val, -3)
         
+        droneHeight = dronePose.position.z_val
+        randSpawnRangeFactor = (droneHeight / 2) + 5
+
+        gunshotPos += airsim.Vector3r(random.uniform(-randSpawnRangeFactor, randSpawnRangeFactor), random.uniform(-randSpawnRangeFactor, randSpawnRangeFactor), random.uniform(-2, 2))
         simSpawnGunshot(client, gunshotPos)
         time.sleep(2)
